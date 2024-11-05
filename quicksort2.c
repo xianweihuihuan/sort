3c9261cfb4cc07c4338d5f758d227d2825f470ff
xianweihuihuan <2907404984@qq.com>
2024-11-06 02:04:14 +0800

    快速排序——挖洞

---
+++ quicksort2.c
@@ -0,0 +1,32 @@
+int _quicksort(int *arr, int left, int right)
+{
+    int hole = left;
+    int key = arr[hole];
+    while (left < right)
+    {
+        while (left < right && arr[right] > key)
+        {
+            right--;
+        }
+        arr[hole] = arr[right];
+        hole = right;
+        while (left < right && arr[left] < key)
+        {
+            left++;
+        }
+        arr[hole] = arr[left];
+        hole = left;
+    }
+    arr[hole] = key;
+    return hole;
+}
+void quicksort(int *arr, int left, int right)
+{
+    if (left >= right)
+    {
+        return;
+    }
+    int key = _quicksort(arr, left, right);
+    quicksort(arr, left, key - 1);
+    quicksort(arr, key + 1, right);
+}
